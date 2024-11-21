9068d4b69fc925115e09210de36f32cc7105cbb0
KuanTa <KuanTa@LinKuanTa>
2024-11-22 00:58:46 +0800

    mvc

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +22,28 @@ public class TodoService {
 	@Transactional
 	public void addTodo(Todo todo) {
 		log.info("Create new todo for user: {}", todo.getTusername());
+		if(todo.getDescription() == null || todo.getDescription().trim().isEmpty()) {
+			throw new RuntimeException("待辦事項，敘述不能為空");
+		}
         todoDao.save(todo);
-        log.info("Created successfully!: {}", todo.getTusername());
+        log.info("Created successfully! user: {}", todo.getTusername());
+	}
+	
+	// 獲取用戶 所有的待辦事項
+	public List<Todo> getUserTodos(String username){
+		log.debug("User Todo list，user: {}", username);
+		return todoDao.findAllByUsername(username);
 	}
 	
 	@Transactional
     public void updateTodo(Todo todo) {
         log.info("Update todo for id: {}", todo.getTid());
+        Todo existtd = todoDao.findById(todo.getTid())
+        		.orElseThrow(() -> new RuntimeException("待辦事項不存在"));
+        if (!existtd.getTusername().equals(todo.getTusername())) {
+            throw new RuntimeException("無權修改此待辦事項");
+        }
+        
         todoDao.update(todo);
         log.info("Updated successfully!: {}", todo.getTid());
     }
@@ -34,9 +51,11 @@ public class TodoService {
 	@Transactional
     public void deleteTodo(Long tid) {
         log.info("Delete todo for id: {}", tid);
+        
+        Todo todo = todoDao.findById(tid)
+        		.orElseThrow(() -> new RuntimeException("待辦事項不存在"));
+        
         todoDao.delete(tid);
         log.info("Deleted successfully!: {}", tid);
     }
-	
-	
 }
